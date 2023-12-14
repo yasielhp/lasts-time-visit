8130d62939e579b89bd34b4b7bc571606c832f5e
Yasiel Hernández Portal <yasielhp@gmail.com>
2023-12-14 10:21:02 +0000

    add new code

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 import { Hono } from "https://deno.land/x/hono@v3.11.7/mod.ts"
-import { serveStatic } from "https://deno.land/x/hono@v3.11.7/middleware.ts"
+import { cors, serveStatic } from "https://deno.land/x/hono@v3.11.7/middleware.ts"
 import { streamSSE } from "https://deno.land/x/hono@v3.11.7/helper/streaming/index.ts"
 
 
@@ -13,7 +13,7 @@ interface LastVisit {
   flag: string
 
 }
-
+app.use(cors())
 app.get('/', serveStatic({ path:'./index.html' }))
 
 
